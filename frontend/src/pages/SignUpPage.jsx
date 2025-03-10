ebc221e30df4aa9518c635877939bcb41be9ba55
hocvn <hocdoth04@gmail.com>
2025-03-10 15:36:50 +0700

    Init frontend

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+const SignUpPage = () => {
+    return (
+        <div>
+        <h1>Sign up Page</h1>
+        <div className="bg-blue-500 text-white p-4">Hello Tailwind</div>
+        </div>
+    )
+};
+
+export default SignUpPage;
