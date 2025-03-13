6f0945a3ca4d96d0e60f1e26a465cba0f6a10640
hocvn <hocdoth04@gmail.com>
2025-03-13 22:48:09 +0700

    fix something that i do not know

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDB } from './lib/db.js';
+import path from "path";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import path from 'path';
+import { connectDB } from "./lib/db.js";
 
-import { app, server } from './lib/socket.js';
-
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
@@ -19,23 +18,25 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "*",
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/..frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
